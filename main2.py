f8ececfd3b742a5dd080c3fedfed4366df88eeab
kevin wafula <wafula@workstation.wafula>
2023-09-24 11:47:49 +0300

    pygame

--- main2.py
+++ main2.py
@@ -14,31 +14,22 @@
 # print(packages.extra.good.best.sigma.FunS())
 # print(FunT())
 
-# import pygame
+import pygame
 
-# run = True
-# width = 400
-# height = 100
-# pygame.init()
-# screen = pygame.display.set_mode((width, height))
-# font = pygame.font.SysFont(None, 48)
-# text = font.render("Welcome to pygame", True, (255, 255, 255))
-# screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
-# pygame.display.flip()
-# while run:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT\
-#         or event.type == pygame.MOUSEBUTTONUP\
-#         or event.type == pygame.KEYUP:
-#             run = False
+run = True
+width = 400
+height = 100
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+font = pygame.font.SysFont(None, 48)
+text = font.render("Welcome to pygame", True, (255, 255, 255))
+screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
+pygame.display.flip()
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT\
+        or event.type == pygame.MOUSEBUTTONUP\
+        or event.type == pygame.KEYUP:
+            run = False
 
 
-# from random import randint
-
-# for i in range(2):
-#     print(randint(1,2), end='')
-
-import math
-
-result = math.e != math.pow(2, 4)
-print(int(result))
