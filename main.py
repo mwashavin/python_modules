93ad089cbb6ad132765438215913e070e74a614a
kevin wafula <wafula@workstation.wafula>
2023-09-24 11:46:31 +0300

    modules and packages

---
+++ main.py
@@ -0,0 +1,10 @@
+# from sys import path
+
+from modules.module import suml, prodl
+
+# path.append('..\\modules')
+
+zeroes = [0 for i in range(5)]
+ones  = [1 for i in range(5)]
+print(suml(zeroes))
+print(prodl(ones))
---
+++ main2.py
@@ -0,0 +1,44 @@
+# from sys import path
+
+# path.append('..\\packages')
+
+# import packages.extra.iota
+# print(packages.extra.iota.FunI())
+
+# from packages.extra.iota import FunI
+# print(FunI())
+
+# import packages.extra.good.best.sigma
+# from packages.extra.good.best.tau import FunT
+
+# print(packages.extra.good.best.sigma.FunS())
+# print(FunT())
+
+# import pygame
+
+# run = True
+# width = 400
+# height = 100
+# pygame.init()
+# screen = pygame.display.set_mode((width, height))
+# font = pygame.font.SysFont(None, 48)
+# text = font.render("Welcome to pygame", True, (255, 255, 255))
+# screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
+# pygame.display.flip()
+# while run:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT\
+#         or event.type == pygame.MOUSEBUTTONUP\
+#         or event.type == pygame.KEYUP:
+#             run = False
+
+
+# from random import randint
+
+# for i in range(2):
+#     print(randint(1,2), end='')
+
+import math
+
+result = math.e != math.pow(2, 4)
+print(int(result))
